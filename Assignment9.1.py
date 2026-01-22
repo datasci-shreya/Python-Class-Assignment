0e6b45f8a2a73ac846b13424914bc76d39089006
datasci-shreya <shreyuborate@gmail.com>
2026-01-22 14:02:23 +0530

    Delete Assignment9.1.py

--- Assignment9.1.py
+++
@@ -1,5 +0,0 @@
-#Write a program which contains one function named as Display() that prints " Jay Ganesh " on console.
-def Display(): #display() is a funtion 
-    print("Jay Ganesh....") #print is used for print message
-
-Display() #call function
