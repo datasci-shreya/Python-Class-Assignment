fd042dd35656f7d3ab521d4088e115f278c4213a
datasci-shreya <shreyuborate@gmail.com>
2026-01-22 14:02:53 +0530

    Delete Assignment9.3.py

--- Assignment9.3.py
+++
@@ -1,7 +0,0 @@
-#Write a program which contains one number and prints square of that number
-#Input - 5
-#Output - 25
-def Square(No): #square() is function with passing one parameter value
-    print("Square is:", No * No) #print square of No
-
-Square(5) #funtion with passing argument 
