7bf3e4cba7063c7eece7183424c60c431f0af437
datasci-shreya <shreyuborate@gmail.com>
2026-01-22 14:02:06 +0530

    Delete Assignment9.4.py

--- Assignment9.4.py
+++
@@ -1,5 +0,0 @@
-#Write a program which accepts one number and prints cube of that number
-def Cube(No): #Cube() is function with passing one parameter value
-    print("Cube is:", No * No) #print Cube of No
-
-Cube(3) #function with passing argument 
