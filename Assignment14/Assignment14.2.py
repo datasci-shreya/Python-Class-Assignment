dd85d52684b5d8bb70129066a5cf7d56e0f8531e
datasci-shreya <shreyuborate@gmail.com>
2026-01-24 00:11:29 +0530

    Update Assignment14.2.py

--- Assignment14/Assignment14.2.py
+++ Assignment14/Assignment14.2.py
@@ -1,12 +1,12 @@
 #Write a lambda function which accept one number and returns cube of that number
-#First method
+
 Cube = lambda No : No * No *No
 No= int(input("Enter a number : "))
 Result = Cube(No)
 print("Cube is : ",Result)
 
 print("_"*15)
-#Second method 
+
 Cube = lambda No : No * No * No
 def main():
     Value = 0
@@ -22,4 +22,5 @@ if __name__ == "__main__":
 #_______________
 #Enter a Number :
 #6
+
 #Cube is :  216
