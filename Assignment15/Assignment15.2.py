302ba2662c0e401feb5a7484a5417dc9e59c0ba8
datasci-shreya <shreyuborate@gmail.com>
2026-01-23 23:48:26 +0530

    Add files via upload

---
+++ Assignment15/Assignment15.2.py
@@ -0,0 +1,15 @@
+#Write a lambda function using filter() which accepts a list of numbers and returns a list of even numbers
+CheckEven = lambda No : No % 2 == 0
+def main():
+    Data = [1,2,3,4,5,6,7,8,9]
+    print("Actual Data is : ",Data)
+
+    FData = list(filter(CheckEven,Data))
+    #FData = list(filter(lambda No : No % 2 == 0,Data))
+    print("Data After Filter is : ",FData)
+
+if __name__ == "__main__":
+    main()
+#Output
+#Actual Data is :  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#Data After Filter is :  [2, 4, 6, 8]
